93230684436e6c1bc19b93d55e1338e357339f85
lucojs <luc-olivier.merson@epitech.eu>
2017-04-30 17:49:21 +0000

    static pages

--- app/controllers/custom_pages_controller.rb
+++ app/controllers/custom_pages_controller.rb
@@ -1,2 +1,19 @@
 class CustomPagesController < ApplicationController
+
+# GET /memberships/new
+
+
+    def new
+    end
+
+    def show
+    end
+    
+    def update
+    end
+
+    def create
+        @
+    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
   resources :messages, except: [:new, :update, :edit]
   get 'contact' => 'messages#new'
 
-  resources :custom_pages
+  resources :custom_pages, except :index
+  
 end
