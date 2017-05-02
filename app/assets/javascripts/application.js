489b70380664427653faea732cbc312d5c3e3d12
Galdan <g.moulinneuf@outlook.fr>
2017-05-03 00:33:36 +0100

    Fixed turbo

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require turbolinks
 //= require materialize-sprockets
 //= require image-picker
-//= require jquery
 //= require jquery_ujs
 //= require_tree .
