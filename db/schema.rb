9902194f03f413661b098289df00f14e7d7c3999
Galdan <g.moulinneuf@outlook.fr>
2017-05-09 19:51:02 +0100

    Schema

---
+++ db/schema.rb
@@ -0,0 +1,72 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20170502131240) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string "login",           null: false
+    t.string "password_digest", null: false
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.string  "name",       null: false
+    t.string  "phone",      null: false
+    t.string  "email",      null: false
+    t.date    "date",       null: false
+    t.string  "membership"
+    t.integer "course_id"
+    t.index ["course_id"], name: "index_bookings_on_course_id"
+  end
+
+  create_table "courses", force: :cascade do |t|
+    t.string  "title",       null: false
+    t.integer "day",         null: false
+    t.string  "time",        null: false
+    t.string  "description", null: false
+    t.string  "instructor",  null: false
+    t.date    "start_date",  null: false
+  end
+
+  create_table "custom_pages", force: :cascade do |t|
+    t.string  "title",                   null: false
+    t.string  "text",                    null: false
+    t.boolean "editable", default: true
+  end
+
+  create_table "custom_pages_pictures", id: false, force: :cascade do |t|
+    t.integer "custom_page_id", null: false
+    t.integer "picture_id",     null: false
+    t.index ["custom_page_id", "picture_id"], name: "index_custom_pages_pictures_on_custom_page_id_and_picture_id"
+    t.index ["picture_id", "custom_page_id"], name: "index_custom_pages_pictures_on_picture_id_and_custom_page_id"
+  end
+
+  create_table "memberships", force: :cascade do |t|
+    t.string "title",       null: false
+    t.string "description", null: false
+    t.float  "price",       null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string   "name",  null: false
+    t.string   "phone", null: false
+    t.string   "email", null: false
+    t.string   "text",  null: false
+    t.datetime "date",  null: false
+  end
+
+  create_table "pictures", force: :cascade do |t|
+    t.string "filename", null: false
+    t.string "alt_text", null: false
+    t.string "caption",  null: false
+  end
+
+end
