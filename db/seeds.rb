8b5a5e7235c2084f8a5a3ddb855224c06c2e32f1
Galdan <g.moulinneuf@outlook.fr>
2017-05-09 18:49:22 +0100

    Fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,14 +4,3 @@ CustomPage.create(title: 'Homepage', text: '', editable: false)
 CustomPage.create(title: 'Planning', text: '', editable: false)
 CustomPage.create(title: 'Memberships', text: '', editable: false)
 CustomPage.create(title: 'Contact us', text: '', editable: false)
-CustomPage.create(title: 'Halloween', text: 'If you subscribe to the Yoga class, you wil die by fear !')
-CustomPage.create(title: 'Christmas', text: 'Merry Christmas !')
-
-Message.create(name: 'Galdan MOULINNEUF', phone: '+33605425326', email: 'g.moulinneuf@outlook.fr', text: 'Hi, my name is Galdan', date: DateTime.now)
-Message.create(name: 'Galdan MOULINNEUF', phone: '+33605425326', email: 'g.moulinneuf@outlook.fr', text: 'Hi, my name is Galdan', date: DateTime.now.midnight)
-
-Membership.create(title: 'Bronze', description: 'Have access to every machines', price: 20)
-Membership.create(title: 'Silver', description: 'Bronze avantages + free eneretic drinks and dietetic food', price: 50)
-Membership.create(title: 'Gold', description: 'Silver avantages + personal trainer', price: 100)
-
-Course.create(title: 'Yoga', day: 5, time: '15:00', description: 'Yoga class', instructor: 'Julius', start_date: DateTime.new(2017, 05, 01).midnight)
